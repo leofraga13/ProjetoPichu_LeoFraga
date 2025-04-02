7ec866a5f28378f08c58655476a5f7b3f562df0c
Leonardo Fraga <fragaleo2002@gmail.com>
2025-04-02 18:15:10 -0300

    atualizacao 02/04

---
+++ projeto/script.js
@@ -0,0 +1,8 @@
+document.getElementById('camisa1').addEventListener('click', function() {
+    window.location.href = 'https://www.saostore.com.br/p/camisa-sao-paulo-i-2425-sn-torcedor-new-balance-masculina-branco+vermelho-39V-0274-024';});
+
+document.getElementById('camisa2').addEventListener('click', function() {
+    window.location.href = 'https://www.saostore.com.br/p/camisa-sao-paulo-ii-2425-sn-torcedor-new-balance-masculina-vermelho+branco-39U-9333-056';});
+
+document.getElementById('camisa3').addEventListener('click', function() {
+    window.location.href = 'https://www.saostore.com.br/p/camisa-sao-paulo-2425-sn-jogador-comemorativa-new-balance-masculina-preto+vermelho-39V-0542-002';});
